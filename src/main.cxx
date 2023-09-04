5c6e96d975ed671695ceb312fd2321ca11ed9337
HALBY <develop@halby24.com>
2023-09-04 15:50:42 +0900

    kansei

--- src/main.cxx
+++ src/main.cxx
@@ -1,7 +1,56 @@
-#include <iostream>
+#include "Common.h"
 
-int main()
+static bool gVerbose = true;
+
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!" << std::endl;
+    FbxManager* lSdkManager = NULL;
+    FbxScene* lScene = NULL;
+    bool lResult;
+
+    // Prepare the FBX SDK.
+    InitializeSdkObjects(lSdkManager, lScene);
+
+    // The example can take a FBX file as an argument.
+	FbxString lFilePath("");
+	for( int i = 1, c = argc; i < c; ++i )
+	{
+		if( FbxString(argv[i]) == "-test" ) gVerbose = false;
+		else if( lFilePath.IsEmpty() ) lFilePath = argv[i];
+	}
+
+	if( lFilePath.IsEmpty() )
+	{
+        lResult = false;
+        FBXSDK_printf("\n\nUsage: ImportScene <FBX file name>\n\n");
+	}
+	else
+	{
+        char* lPath = NULL;
+        FbxAnsiToUTF8(lFilePath.Buffer(), lPath, NULL);
+        lFilePath = lPath;
+
+		FBXSDK_printf("\n\nFile: %s\n\n", lFilePath.Buffer());
+		lResult = LoadScene(lSdkManager, lScene, lFilePath.Buffer());
+	}
+
+    if(lResult == false)
+    {
+        FBXSDK_printf("\n\nAn error occurred while loading the scene...");
+    }
+    else 
+    {
+        lResult = SaveScene(lSdkManager, lScene, lFilePath.Buffer());
+        if(lResult == false)
+        {
+            FBXSDK_printf("\n\nAn error occurred while saving the scene...\n");
+            DestroySdkObjects(lSdkManager, lResult);
+            return 0;
+        }
+    }
+
+    // Destroy all objects created by the FBX SDK.
+    DestroySdkObjects(lSdkManager, lResult);
+
     return 0;
 }
